381e990c8f81c515fc56b6ce56c014ac104659cb
BuildTools <unconfigured@null.spigotmc.org>
2020-06-16 09:05:02 -0400

    [auto] build save

--- src/hvh/org/Hub.java
+++ src/hvh/org/Hub.java
@@ -77,34 +77,37 @@ public class Hub {
     
     public void parseCommand(Player normie, List<String> args) {
         HvHPlayer player = new HvHPlayer(normie, games);
-        String cmd = args.remove(0);
         
-        switch (cmd) {
-            case "join":
-                // check if player already in game
-                if (player.inGame()) {
-                    help(player, "sir. you are already in a game");
-                } else if (args.size() > 0) {
-                    // add player to a game
-                    String team = args.get(0);                                      // team the player wants to be on
-                    player.joinTeam(team);                                      // set the players team
-                    player.joinGame();                                         // add player to game lobby
-		        } else {
-                    help(player, "you must specify a team: /hvh join [hunter/hunted]");
-		        }
-		        return;
-	        case "leave":
-                player.leaveGame();
-                return;
-            case "admin":
-                parseAdminCommand(player, args);
-                return;
-            default:
-                help(player, "unknown command " + cmd);
-        }
+        if (args.size() >= 1) {
+            String cmd = args.remove(0);
         
-		help(player, "available commands: join, spawn, setspawn");
+            switch (cmd) {
+                case "join":
+                    // check if player already in game
+                    if (player.inGame()) {
+                        help(player, "sir. you are already in a game");
+                    } else if (args.size() > 0) {
+                        // add player to a game
+                        String team = args.get(0);                                      // team the player wants to be on
+                        player.joinTeam(team);                                      // set the players team
+                        player.joinGame();                                         // add player to game lobby
+		            } else {
+                        help(player, "you must specify a team: /hvh join [hunter/hunted]");
+	    	        }
+		            return;
+	            case "leave":
+                    player.leaveGame();
+                    return;
+                case "admin":
+                    parseAdminCommand(player, args);
+                    return;
+                default:
+                    help(player, "unknown command " + cmd);
+            }
     }
+    
+    help(player, "available commands: join, spawn, setspawn");
+}
     
     public void setSpawn(HvHPlayer hplayer) {
         setSpawn(hplayer.player);
