fa8679e8cf0b318032df0e7542e59c91948c9684
BuildTools <unconfigured@null.spigotmc.org>
2020-06-16 15:23:02 -0400

    [auto] build save

--- src/hvh/org/GameEndEvent.java
+++ src/hvh/org/GameEndEvent.java
@@ -3,6 +3,7 @@ package hvh.org;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.Location;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
